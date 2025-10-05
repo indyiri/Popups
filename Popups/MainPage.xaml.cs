1275fb5cafc101507159fce9fd707a223e25ea9b
Indy Iri <12402665@student.pxl.be>
2025-10-06 00:24:13 +0200

    labo afgewerkt

--- Popups/MainPage.xaml.cs
+++ Popups/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Threading.Tasks;
+
 namespace Popups
 {
     public partial class MainPage : ContentPage
@@ -13,6 +14,44 @@ namespace Popups
         {
             return Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
         }
-    }
 
+        private async void OnButtonClicked(object sender, EventArgs e)
+        {
+            string input = await DisplayPromptAsync("Toevoegen", "Geef een naam:");
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                await DisplayAlert("Fout", "U heeft geen naam ingegeven, probeer opnieuw!", "OK");
+            }
+            else
+            {
+                var button = new Button
+                {
+                    Text = input,
+                    BackgroundColor = GetRandomColor(),
+                    TextColor = Colors.White,
+                    CornerRadius = 10,
+                    Margin = new Thickness(0),
+                    Padding = new Thickness(20),
+                    FontSize = 18,
+                };
+
+                button.Clicked += DynamicButton_Clicked;
+
+                flexLayout.Children.Add(button);
+            }
+        }
+
+        private async void DynamicButton_Clicked(object sender, EventArgs e)
+        {
+            if (sender is Button button)
+            {
+                bool confirm = await DisplayAlert("Verwijderen?", $"Wil je '{button.Text}' verwijderen?", "Ja", "Nee");
+                if (confirm)
+                {
+                    flexLayout.Children.Remove(button);
+                }
+            }
+        }
+    }
 }
